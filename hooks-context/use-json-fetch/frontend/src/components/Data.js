c84a16011eb3471518d538824564af4fe21166fd
Viktor <79064527660@yandex.ru>
2020-04-20 17:01:07 +0300

    fixed Data

--- hooks-context/use-json-fetch/frontend/src/components/Data.js
+++ hooks-context/use-json-fetch/frontend/src/components/Data.js
@@ -2,12 +2,15 @@ import React from "react";
 import useJsonFetch from "../hooks/useJsonFetch";
 
 export default ({url}) => {
-    const [data, loading] = useJsonFetch(`${process.env.REACT_APP_BASE_URL}${url}`);
+    const [data, loading, error] = useJsonFetch(`${process.env.REACT_APP_BASE_URL}${url}`);
     if(loading) {
         return <p>Loading...</p>
     }
     if (data) {
         return <p>{data.status}</p>
+    }
+    if (error) {
+        return <p>{error.message}</p>
     }
     return null
 }
